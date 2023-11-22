ae861dedd62fd30d5323669d1ca4b7275727de75
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2023-11-21 19:19:20 -0800

    Refactored and better logging

--- src/ak_sap/Database/tables.py
+++ src/ak_sap/Database/tables.py
@@ -9,6 +9,7 @@ class Table:
     def __init__(self, mySapObject) -> None:
         self.mySapObject = mySapObject
         self.SapModel = self.mySapObject.SapModel
+        self.DatabaseTables = self.SapModel.DatabaseTables
         log.debug('Instance of `Table` module initialized.')
 
     def __str__(self) -> str:
@@ -18,36 +19,50 @@ class Table:
         """Returns all of the tables along with their import type and 
         indicates if any data is available in the model to fill the table."""
         log.debug('Getting a list of all Tables')
-        _table_data = self.SapModel.DatabaseTables.GetAllTables()
+        _table_data = []
         tables = []
-        for _TableKey, _TableName, _ImportType, _IsEmpty in zip(*_table_data[1:-1]):
-            _import = typing.get_args(ImportType_Literals)[_ImportType]
-            tables.append(DatabaseTable(
-                TableKey=_TableKey, TableName=_TableName, ImportType=_import, IsEmpty=_IsEmpty))
-        log.debug(f'{len(tables)} tables found')
+        try:
+            _table_data = self.DatabaseTables.GetAllTables()
+            assert _table_data[-1] == 0
+            for _TableKey, _TableName, _ImportType, _IsEmpty in zip(*_table_data[1:-1]):
+                _import = typing.get_args(ImportType_Literals)[_ImportType]
+                tables.append(DatabaseTable(
+                    TableKey=_TableKey, TableName=_TableName, ImportType=_import, IsEmpty=_IsEmpty))
+            log.debug(f'{len(tables)} tables found')
+        except Exception as e:
+            log.critical(str(e) + f'Return data: {_table_data}')
         return tables
     
     def list_available(self) -> list[DatabaseTable]:
         """Returns all available tables along with their import type."""
         log.debug('Getting a list of all available Tables')
-        _table_data = self.SapModel.DatabaseTables.GetAvailableTables()
         tables = []
-        for _TableKey, _TableName, _ImportType in zip(*_table_data[1:-1]):
-            _import = typing.get_args(ImportType_Literals)[_ImportType]
-            tables.append(DatabaseTable(
-                TableKey=_TableKey, TableName=_TableName, ImportType=_import, IsEmpty=False))
-        log.debug(f'{len(tables)} tables found')
+        _table_data = []
+        try:
+            _table_data = self.DatabaseTables.GetAvailableTables()
+            assert _table_data[-1] == 0
+            for _TableKey, _TableName, _ImportType in zip(*_table_data[1:-1]):
+                _import = typing.get_args(ImportType_Literals)[_ImportType]
+                tables.append(DatabaseTable(
+                    TableKey=_TableKey, TableName=_TableName, ImportType=_import, IsEmpty=False))
+            log.debug(f'{len(tables)} tables found')
+        except Exception as e:
+            log.critical(str(e) + f'Return data: {_table_data}')
         return tables
     
     def get_table_fields(self, TableKey: str) -> list[FieldData]:
         """Returns the available fields in a specified table."""
         log.debug(f'Extracting available fields in `TableKey`: {TableKey}')
-        _table_data = self.SapModel.DatabaseTables.GetAllFieldsInTable(TableKey)
-        tables = []
-        for _FieldKey, _FieldName, _Description, _UnitsStr, _isImportable in zip(*_table_data[2:-1]):
-            tables.append(FieldData(
-                FieldKey=_FieldKey, FieldName=_FieldName, Description=_Description, UnitsStr=_UnitsStr, isImportable=_isImportable
-            ))
+        _table_data, tables = [], []
+        try:
+            _table_data = self.DatabaseTables.GetAllFieldsInTable(TableKey)
+            assert _table_data[-1] == 0
+            for _FieldKey, _FieldName, _Description, _UnitsStr, _isImportable in zip(*_table_data[2:-1]):
+                tables.append(FieldData(
+                    FieldKey=_FieldKey, FieldName=_FieldName, Description=_Description, UnitsStr=_UnitsStr, isImportable=_isImportable
+                ))
+        except Exception as e:
+            log.critical(str(e) + f'Return data: {_table_data}')
         return tables
     
     def data(self, TableKey: str) -> pd.DataFrame:
@@ -57,7 +72,7 @@ class Table:
         log.debug(f'Extracting data for TableKey: {TableKey}')
         _data: list = []
         try:
-            _data = self.SapModel.DatabaseTables.GetTableForDisplayArray(TableKey,'','')
+            _data = self.DatabaseTables.GetTableForDisplayArray(TableKey,'','')
             assert _data[-1] == 0
             df = _array_to_pandas(headers=_data[2], array=_data[4])
             log.debug(f'Info of retrieved dataframe: \n{df.info(verbose=True)}')
@@ -67,9 +82,15 @@ class Table:
             return pd.DataFrame()
     
     def update(self, TableKey: str, data: pd.DataFrame, apply: bool=True):
+        """Update the database table value"""
         log.debug(f'Updating the values for TableKey: {TableKey}')
         TableVersion: int = 1
-        self.SapModel.DatabaseTables.SetTableForEditingArray(TableKey,TableVersion,tuple(data.columns), len(data), flatten_dataframe(data))
+        *_, ret = self.DatabaseTables.SetTableForEditingArray(TableKey,TableVersion,tuple(data.columns), len(data), flatten_dataframe(data))
+        try:
+            assert ret == 0
+        except Exception as e:
+            log.critical(str(e) + f'Return Data: {_}')
+            return
         if apply:
             self.apply()
             
@@ -80,20 +101,19 @@ class Table:
         that can be interactively imported when the model is locked will be imported. 
         """
         log.debug('Applying changes to database')
-        ret = []
+        NumFatalErrors, NumErrorMsgs, NumWarnMsgs, NumInfoMsgs, ImportLog, ret = self.DatabaseTables.ApplyEditedTables(True)
         try:
-            ret = self.SapModel.DatabaseTables.ApplyEditedTables(True)
-            if ret[0] != 0 or ret[1] != 0:
-                log.error(ret[-2])
-            elif ret[2] != 0:
-                log.warning(ret[-2])
-            elif ret[3] != 0:
-                log.info(ret[-2])
+            if NumFatalErrors != 0 or NumErrorMsgs != 0:
+                log.error(ImportLog)
+            elif NumWarnMsgs != 0:
+                log.warning(ImportLog)
+            elif NumInfoMsgs != 0:
+                log.info(ImportLog)
             else:
-                log.debug(ret[-2])
-            assert ret[-1] == 0
+                log.debug(ImportLog)
+            assert ret == 0
         except Exception as e:
-            log.critical(str(e) + f'Return data: {ret}')
+            log.critical(str(e) + f'Import Log: {ImportLog}')
         
 def _array_to_pandas(headers: tuple[str], array: tuple) -> pd.DataFrame:
     """Given the table headers as tuple and table data as a single tuple;
