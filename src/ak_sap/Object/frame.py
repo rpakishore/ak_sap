fba0b65097130dd8cf914d6f05ad123413cdf800
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2024-03-07 11:52:40 -0800

    test

--- src/ak_sap/Object/frame.py
+++ src/ak_sap/Object/frame.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from ak_sap.utils import log
 from ak_sap.utils.decorators import smooth_sap_do
 from .helper import MasterObj
@@ -6,19 +8,24 @@ class Frame(MasterObj):
     def __init__(self, mySapObject) -> None:
         super().__init__(mySapObject=mySapObject, ElemObj=mySapObject.SapModel.FrameObj)
         
+        self.EditFrame = mySapObject.SapModel.EditFrame
         self.Prop = Prop(mySapObject=mySapObject)
     
     @smooth_sap_do
     def get_section(self, frame_name: str) -> str:
-        self.check_element_legal(name=frame_name)
+        self.check_obj_legal(name=frame_name)
         _ret = self.ElemObj.GetSection(frame_name)
         return (_ret[0], _ret[-1]) # type: ignore
     
     @smooth_sap_do
     def get_points(self, frame_name: str) -> tuple[str]:
         """retrieves the names of the point objects at each end of a specified frame object."""
-        #self.check_element_legal(name=frame_name)
+        #self.check_obj_legal(name=frame_name)
         return self.ElemObj.GetPoints(frame_name)
+    
+    @smooth_sap_do
+    def divide_by_distance(self, frame_name: str, dist: float, Iend: bool=True, num_divisions: int=1):
+        return self.EditFrame.DivideAtDistance(frame_name, dist, Iend)
 
 class Prop:
     def __init__(self, mySapObject) -> None:
